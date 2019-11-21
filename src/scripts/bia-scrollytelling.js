404a99a9e646bf45fe6d234d7bed7fefeff467ec
Kasturi Pananjady <kasturipananjady@dyn-160-39-203-148.dyn.columbia.edu>
2019-11-21 17:11:17 -0500

    some carousel updates

--- src/scripts/bia-scrollytelling.js
+++ src/scripts/bia-scrollytelling.js
@@ -1,17 +1,18 @@
 import * as d3 from 'd3'
 /* Hide all images on load */
 const images = d3.selectAll('.image-stepper img')
-images.style('display', 'none')
+images.classed('hide-image', true)
+
 /* Show the first one */
-d3.select('.image-stepper img:first-child').style('display', 'block')
+d3.select('.image-stepper img:first-child').classed('hide-image', false)
 d3.selectAll('.image-stepper .step').each(d => {
   console.log('this exists')
 })
 /* When you hit a step... */
 d3.selectAll('.image-stepper .step').on('stepin', function() {
   // Hide all the images
-  images.style('display', 'none')
+  images.classed('hide-image', true)
   // Show one of the images
   const imageId = d3.select(this).attr('data-show-selector')
-  d3.selectAll(imageId).style('display', 'block')
+  d3.selectAll(imageId).classed('hide-image', false)
 })
