a50f54b14d2c45833b60dbcad7c6316617a85fac
kpananjady <51126772+kpananjady@users.noreply.github.com>
2020-09-05 19:41:25 -0400

    testings widths

--- src/scripts/flow.js
+++ src/scripts/flow.js
@@ -17,119 +17,125 @@ const svg = d3
 
 //tier one
 
-svg.append('text').text('835,000 people have applied for unemployment benefits').attr('alignment-baseline', 'middle').attr('y',-105).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
-svg.append('text').text('since the start of the pandemic.').attr('alignment-baseline', 'middle').attr('y',-85).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
+svg.append('text').text('835,000 people have applied for unemployment benefits').attr('alignment-baseline', 'middle').attr('y',-105).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0).attr('class', 'title').attr('id', 'hed')
+svg.append('text').text('since the start of the pandemic.').attr('alignment-baseline', 'middle').attr('y',-85).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0).attr('class', 'title').attr('id', 'hed')
 
-svg.append('text').text("If you were one of them on August 15, here's the backlog you'd face.").attr('alignment-baseline', 'middle').attr('y',-55).attr('font-size', '15px').attr('font-weight', 5).attr('x', 0)
-
-svg.append('text').text('0').attr('alignment-baseline', 'middle').attr('y',-35).attr('font-size', '20px').attr('font-weight', 5).attr('x', -85).attr('class', 'wait').attr('fill', 'blue').attr('opacity', 0.5)
-svg.append('text').text('Weeks').attr('alignment-baseline', 'middle').attr('y',-15).attr('font-size', '20px').attr('font-weight', 5).attr('x', -90).attr('class', 'wait2')
+svg.append('text').text("We don't know how many have been approved. But some 26,000 were waiting").attr('alignment-baseline', 'middle').attr('y',-45).attr('font-size', '15px').attr('font-weight', 5).attr('x', 0).attr('class', 'title').attr('id', 'subhed')
+svg.append('text').text("in various steps of the process in August.").attr('alignment-baseline', 'middle').attr('y',-25).attr('font-size', '15px').attr('font-weight', 5).attr('x', 0).attr('class', 'title').attr('id', 'subhed')
 
 
-svg.append('text').text("The Dept of Labor processes your claim.").attr('alignment-baseline', 'middle').attr('y',height/5-30).attr('font-size', '12px').attr('font-weight', 5).attr('x', 45)
-svg.append('text').text("After your employer is given 5-7 days to respond, the agency looks at your application.").attr('alignment-baseline', 'middle').attr('y',height/5-15).attr('font-size', '10px').attr('font-weight', 5).attr('x', 45)
+svg.append('text').text("If you applied last month, here's the backlog you'd face.").attr('alignment-baseline', 'middle').attr('y',10).attr('font-size', '15px').attr('font-weight', 5).attr('x', 0).attr('id', 'subhed')
 
-svg.append('text').text("If they decide you need a hearing, you need to wait for a date.").attr('alignment-baseline', 'middle').attr('y',2*height/5-30).attr('font-size', '12px').attr('font-weight', 5).attr('x', 45)
-svg.append('text').text("The agency may determine that it needs more information to see if you qualify.").attr('alignment-baseline', 'middle').attr('y',2*height/5-15).attr('font-size', '10px').attr('font-weight', 5).attr('x', 45)
-
-svg.append('text').text("You receive your decision 5-7 days after your hearing.").attr('alignment-baseline', 'middle').attr('y',3*height/5-30).attr('font-size', '12px').attr('font-weight', 5).attr('x', 45)
-svg.append('text').text("It is unclear how many people are approved within the state.").attr('alignment-baseline', 'middle').attr('y',3*height/5-15).attr('font-size', '10px').attr('font-weight', 5).attr('x', 45)
+svg.append('text').text('0').attr('alignment-baseline', 'middle').attr('y',0).attr('font-size', '20px').attr('font-weight', 5).attr('x', -75).attr('class', 'wait').attr('fill', '#072F5F').attr('opacity', 0.5)
+svg.append('text').text('Weeks').attr('alignment-baseline', 'middle').attr('y',20).attr('font-size', '20px').attr('font-weight', 5).attr('x', -80).attr('class', 'wait2')
 
 
-svg.append('text').text("Appealing to the Referee Section").attr('alignment-baseline', 'middle').attr('y',4*height/5-30).attr('font-size', '12px').attr('font-weight', 5).attr('x', 45)
-svg.append('text').text("If you want to appeal the decision, you appear before the referee section").attr('alignment-baseline', 'middle').attr('y',4*height/5-15).attr('font-size', '10px').attr('font-weight', 5).attr('x', 45)
+svg.append('text').text("The Dept of Labor processes your claim.").attr('alignment-baseline', 'middle').attr('y',height/5-30).attr('font-size', '12px').attr('font-weight', 5).attr('x', 45).attr('class','steps')
+svg.append('text').text("After your employer is given 5-7 days to respond, the agency looks at your application.").attr('alignment-baseline', 'middle').attr('y',height/5-15).attr('font-size', '10px').attr('font-weight', 5).attr('x', 45).attr('x', 45).attr('class','steps')
 
-svg.append('text').text("Appealing to the Board of Review").attr('alignment-baseline', 'middle').attr('y',height-30).attr('font-size', '12px').attr('font-weight', 5).attr('x', 45)
-svg.append('text').text("Otherwise, you appear before the Board of Appeals.").attr('alignment-baseline', 'middle').attr('y',height-15).attr('font-size', '10px').attr('font-weight', 5).attr('x', 45)
+svg.append('text').text("If they need more info, you'll need to wait for a hearing date.").attr('alignment-baseline', 'middle').attr('y',2*height/5-30).attr('font-size', '12px').attr('font-weight', 5).attr('x', 45).attr('x', 45).attr('class','steps')
+svg.append('text').text("1 in 50 people who applied are stuck on the adjudication step of the process.").attr('alignment-baseline', 'middle').attr('y',2*height/5-15).attr('font-size', '10px').attr('font-weight', 5).attr('x', 45).attr('x', 45).attr('class','steps')
+
+svg.append('text').text("Appealing: Part 1").attr('alignment-baseline', 'middle').attr('y',3*height/5-30).attr('font-size', '12px').attr('font-weight', 5).attr('x', 45).attr('x', 45).attr('class','steps')
+svg.append('text').text("The Appeals Division has two parts; the lower one is called the Referee Section").attr('alignment-baseline', 'middle').attr('y',3*height/5-15).attr('font-size', '10px').attr('font-weight', 5).attr('x', 45).attr('x', 45).attr('class','steps')
+
+
+svg.append('text').text("Appealing: Part 2").attr('alignment-baseline', 'middle').attr('y',4*height/5-30).attr('font-size', '12px').attr('font-weight', 5).attr('x', 45).attr('x', 45).attr('class','steps')
+svg.append('text').text("The Referee Section's decisions can be overturned by the Board of Review.").attr('alignment-baseline', 'middle').attr('y',4*height/5-15).attr('font-size', '10px').attr('font-weight', 5).attr('x', 45).attr('x', 45).attr('class','steps')
+
+svg.append('text').text("You receive your final decision.").attr('alignment-baseline', 'middle').attr('y',height-30).attr('font-size', '12px').attr('font-weight', 5).attr('x', 45).attr('x', 45).attr('class','steps')
+svg.append('text').text("If you have a hearing and go through the entire appeal process, you're looking at 27 weeks.").attr('alignment-baseline', 'middle').attr('y',height-15).attr('font-size', '10px').attr('font-weight', 5).attr('x', 45).attr('x', 45).attr('class','steps')
 
 // key
 
 svg.append('text').text('160 applications').attr('alignment-baseline', 'middle').attr('y',height+30).attr('font-size', '10px').attr('font-weight', 5).attr('x', -75)
-svg.append('text').text('Unknown').attr('alignment-baseline', 'middle').attr('y',height+40).attr('font-size', '10px').attr('font-weight', 5).attr('x', -75)
+// svg.append('text').text('Unknown').attr('alignment-baseline', 'middle').attr('y',height+40).attr('font-size', '10px').attr('font-weight', 5).attr('x', -75)
 svg.append('text').text('Your application').attr('alignment-baseline', 'middle').attr('y',height+50).attr('font-size', '10px').attr('font-weight', 5).attr('x', -75)
-svg.append('text').text('300 applications').attr('alignment-baseline', 'middle').attr('y',height+60).attr('font-size', '10px').attr('font-weight', 5).attr('x', -75)
+// svg.append('text').text('300 applications').attr('alignment-baseline', 'middle').attr('y',height+60).attr('font-size', '10px').attr('font-weight', 5).attr('x', -75)
 
-svg.append('circle').attr('cy',height+30).attr('font-weight', 5).attr('cx', -85).attr('r', 5).attr('opacity', 0.5)
-svg.append('circle').attr('cy',height+40).attr('font-weight', 5).attr('cx', -85).attr('r', 5).attr('opacity', 0.5).attr('fill', 'white').attr('stroke', 'grey')
-svg.append('circle').attr('cy',height+50).attr('font-weight', 5).attr('cx', -85).attr('r', 5).attr('opacity', 0.5).attr('fill','blue')
-svg.append('circle').attr('cy',height+60).attr('cx', -85).attr('r', 2).attr('opacity', 0.5)
+svg.append('circle').attr('cy',height+30).attr('font-weight', 5).attr('cx', -85).attr('r', 5).attr('opacity', 0.5)    .attr('fill', '#c24e00')
+
+// svg.append('circle').attr('cy',height+40).attr('font-weight', 5).attr('cx', -85).attr('r', 5).attr('opacity', 0.5).attr('fill', 'white').attr('stroke', 'grey')
+svg.append('circle').attr('cy',height+50).attr('font-weight', 5).attr('cx', -85).attr('r', 5).attr('opacity', 0.5).attr('fill','#072F5F')
+// svg.append('circle').attr('cy',height+60).attr('cx', -85).attr('r', 2).attr('opacity', 0.5)
 
 
 
-function draw(width_var, radius_1, radius_2){
+function draw(width_var, radius_2, margin_var){
 
-  console.log(width_var)/(0.5+radius_1*2)
-var i;
-for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
-    svg
-    .append('circle')
-    .attr('class', 'bubble')
-    .attr('cy', -30)
-    .attr('cx', 5*i)
-    .attr('r', radius_1)
-    .attr('opacity', '0.5')
+//   console.log(width_var)/(0.5+radius_1*2)
+// var i;
+// for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
+//     svg
+//     .append('circle')
+//     .attr('class', 'bubble')
+//     .attr('cy', -30)
+//     .attr('cx', space_var*i)
+//     .attr('r', radius_1)
+//     .attr('opacity', '0.5')
 
-}
+// }
 
-var i;
-for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
-    svg
-    .append('circle')
-    .attr('class', 'bubble')
-    .attr('cy', -20)
-    .attr('cx', 5*i)
-    .attr('r', radius_1)
-    .attr('opacity', '0.5')
+// var i;
+// for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
+//     svg
+//     .append('circle')
+//     .attr('class', 'bubble')
+//     .attr('cy', -20)
+//     .attr('cx', space_var*i)
+//     .attr('r', radius_1)
+//     .attr('opacity', '0.5')
 
-}
+// }
 
-var i;
-for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
-    svg
-    .append('circle')
-    .attr('class', 'bubble')
-    .attr('cy', -10)
-    .attr('cx', 5*i)
-    .attr('r', radius_1)
-    .attr('opacity', '0.5')
+// var i;
+// for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
+//     svg
+//     .append('circle')
+//     .attr('class', 'bubble')
+//     .attr('cy', -10)
+//     .attr('cx', space_var*i)
+//     .attr('r', radius_1)
+//     .attr('opacity', '0.5')
 
-}
+// }
 
-  var i;
-    for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
-        svg
-        .append('circle')
-        .attr('class', 'bubble')
-        .attr('cy', 0)
-        .attr('cx', 5*i)
-        .attr('r', radius_1)
-        .attr('opacity', '0.5')
+//   var i;
+//     for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
+//         svg
+//         .append('circle')
+//         .attr('class', 'bubble')
+//         .attr('cy', 0)
+//         .attr('cx', space_var*i)
+//         .attr('r', radius_1)
+//         .attr('opacity', '0.5')
 
-}
+// }
 
-var i;
-for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
-    svg
-    .append('circle')
-    .attr('class', 'bubble')
-    .attr('cy', 10)
-    .attr('cx', 5*i)
-    .attr('r', radius_1)
-    .attr('opacity', '0.5')
+// var i;
+// for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
+//     svg
+//     .append('circle')
+//     .attr('class', 'bubble')
+//     .attr('cy', 10)
+//     .attr('cx', space_var*i)
+//     .attr('r', radius_1)
+//     .attr('opacity', '0.5')
+//     .attr('fill', 'grey')
 
-}
+// }
 
-var i;
-for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
-    svg
-    .append('circle')
-    .attr('class', 'bubble')
-    .attr('cy', 20)
-    .attr('cx', 5*i)
-    .attr('r', 2)
-    .attr('opacity', '0.5')
+// var i;
+// for (i = 0; i < (width_var)/(0.5+radius_1*2); i++) {
+//     svg
+//     .append('circle')
+//     .attr('class', 'bubble')
+//     .attr('cy', 20)
+//     .attr('cx', space_var*i)
+//     .attr('r', 2)
+//     .attr('opacity', '0.5')
 
-}
+// }
 
 
 //tier two
@@ -143,16 +149,17 @@ if (balls_needed < width_var/(radius_2*2)){
     .append('circle')
     .attr('class', 'bubble')
     .attr('cy', height/5)
-    .attr('cx', 50+radius_2*2*i)
+    .attr('cx', margin_var+radius_2*2*i)
     .attr('r', radius_2)
     .attr('opacity', '0.5')
+    .attr('fill', '#c24e00')
 
     }
 }
 //tier three
 
 var balls_needed = 100;
-console.log(width_var/(radius_2*2))
+// console.log(width_var/(radius_2*2))
 var rows = balls_needed/(width_var/(radius_2*2));
 
 var i;
@@ -161,9 +168,10 @@ for (i = 0; i < width_var/(radius_2*2); i++) {
     .append('circle')
     .attr('class', 'bubble')
     .attr('cy', 2*height/5)
-    .attr('cx', 50+radius_2*2*i)
+    .attr('cx', margin_var+radius_2*2*i)
     .attr('r', radius_2)
     .attr('opacity', '0.5')
+    .attr('fill', '#c24e00')
 
 
 }
@@ -174,9 +182,10 @@ for (i = 0; i < width_var/(radius_2*2); i++) {
     .append('circle')
     .attr('class', 'bubble')
     .attr('cy', 2*height/5+10)
-    .attr('cx', 50+radius_2*2*i)
+    .attr('cx', margin_var+radius_2*2*i)
     .attr('r', radius_2)
     .attr('opacity', '0.5')
+    .attr('fill', '#c24e00')
 
 
 }
@@ -187,103 +196,114 @@ for (i = 0; i < 10; i++) {
     .append('circle')
     .attr('class', 'bubble')
     .attr('cy', 2*height/5+20)
-    .attr('cx', 50+radius_2*2*i)
+    .attr('cx', margin_var+radius_2*2*i)
     .attr('r', radius_2)
     .attr('opacity', '0.5')
+    .attr('fill', '#c24e00')
 
 
 }
 
 //tier four
 var i;
-for (i = 0; i <  width_var/(radius_2*2); i++) {
+for (i = 0; i < 22; i++) {
     svg
     .append('circle')
     .attr('class', 'bubble')
     .attr('cy', 3*height/5)
-    .attr('cx', 50+radius_2*2*i)
+    .attr('cx', margin_var+radius_2*2*i)
     .attr('r', radius_2)
     .attr('opacity', '0.5')
-    .attr('fill', 'white')
-    .attr('stroke', 'grey')
+    .attr('fill', '#072F5F')
+    .attr('fill', '#c24e00')
 
 }
 
 //tier five
 var i;
-for (i = 0; i < 22; i++) {
-    svg
-    .append('circle')
-    .attr('class', 'bubble')
-    .attr('cy', 4*height/5)
-    .attr('cx', 50+radius_2*2*i)
-    .attr('r', radius_2)
-    .attr('opacity', '0.5')
-
-}
-
-//tier 6
-var i;
 for (i = 0; i < 2; i++) {
     svg
     .append('circle')
     .attr('class', 'bubble')
-    .attr('cy', height)
-    .attr('cx', 50+radius_2*2*i)
+    .attr('cy', 4*height/5)
+    .attr('cx', margin_var+radius_2*2*i)
     .attr('r', radius_2)
     .attr('opacity', '0.5')
-
-
-    }
+    .attr('fill', '#c24e00')
 
 }
 
-draw(width, 2,5)
+//tier 6
+// var i;
+// for (i = 0; i < 2; i++) {
+//     svg
+//     .append('circle')
+//     .attr('class', 'bubble')
+//     .attr('cy', height)
+//     .attr('cx', margin_var+radius_2*2*i)
+//     .attr('r', radius_2)
+//     .attr('opacity', '0.5')
+//     .attr('fill', '#c24e00')
+
+
+//     }
+
+}
+
+draw(width, 5,50)
 
   d3.select('#toggle').on('click', () => {
 
+    svg.select('.fall_ball').remove()
+
     svg
     .append('circle')
-    .attr('class', 'bubble')
+    .attr('class', 'fall_ball')
     .attr('cy', 0)
     .attr('cx', 0)
     .attr('r', 2)
     .attr('opacity', '0.5')
     .transition()
     .duration(1000)
-    .ease(d3.easeBounce)
+    .ease(d3.easeElastic)
     .attr('r', 5)
     .attr('cy', height/5)
     .attr('cx', 0)
-    .attr('fill', 'blue')
+    .attr('stroke', '#072F5F')
+    .attr('fill', '#072F5F')
     .transition()
     .duration(3000)
     .ease(d3.easeBounce)
     .attr('r', 5)
     .attr('cy', 2*height/5)
     .attr('cx', 0)
-    .attr('fill', 'blue')
-    .transition()
+    .attr('stroke', '#072F5F')
+ .transition()
     .duration(12000)
     .ease(d3.easeBounce)
     .attr('r', 5)
     .attr('cy', 3*height/5)
     .attr('cx', 0)
-    .attr('fill', 'blue')
+    .attr('stroke', '#072F5F')
+
     .transition()
     .duration(10000)
     .ease(d3.easeBounce)
     .attr('r', 5)
     .attr('cy', 4*height/5)
     .attr('cx', 0)
-    .attr('fill', 'blue')
+    .attr('stroke', '#072F5F')
+    .attr('fill', '#072F5F')
+
     .transition()
     .duration(6000)
     .ease(d3.easeBounce)
     .attr('r', 5)
-    .attr('cy', height)
+    .attr('cy', height-25)
     .attr('cx', 0)
-    .attr('fill', 'blue')
+    .attr('stroke', '#072F5F')
+    .attr('fill', '#072F5F')
+
 
 
     svg.select('.wait')
@@ -319,6 +339,73 @@ draw(width, 2,5)
     .ease(d3.easeBounce)
 
     .text('27')
-    .attr('y', height)
+    .attr('y', height-25)
 
       })
+
+
+      function render() {
+        const svgContainer = svg.node().closest('div')
+        const svgWidth = svgContainer.offsetWidth
+        // Do you want it to be full height? Pick one of the two below
+        const svgHeight = height + margin.top + margin.bottom
+        // const svgHeight = window.innerHeight
+    
+        const actualSvg = d3.select(svg.node().closest('svg'))
+        actualSvg.attr('width', svgWidth).attr('height', svgHeight)
+    
+        const newWidth = svgWidth - margin.left - margin.right
+        const newHeight = svgHeight - margin.top - margin.bottom
+
+        svg.selectAll('circle').remove()
+        console.log(newWidth)
+
+        
+        svg.append('circle').attr('cy',height+30).attr('font-weight', 5).attr('cx', -85).attr('r', 5).attr('opacity', 0.5)    .attr('fill', '#c24e00')
+        
+        // svg.append('circle').attr('cy',height+40).attr('font-weight', 5).attr('cx', -85).attr('r', 5).attr('opacity', 0.5).attr('fill', 'white').attr('stroke', 'grey')
+        svg.append('circle').attr('cy',height+50).attr('font-weight', 5).attr('cx', -85).attr('r', 5).attr('opacity', 0.5).attr('fill','#072F5F')
+        // svg.append('circle').attr('cy',height+60).attr('cx', -85).attr('r', 2).attr('opacity', 0.5)
+        
+
+
+        if (newWidth >  355) {
+            draw(width, 5,50)
+            svg.selectAll('#hed').attr('font-size', '20px')
+            svg.selectAll('#subhed').attr('font-size', '15px')
+
+  
+          } else if (newWidth > 300){
+  
+            draw(width, 5,30)
+            svg.selectAll('.steps').attr('x', 30)
+            svg.selectAll('.title').attr('x', 0)
+            svg.selectAll('#hed').attr('font-size', '17px')
+            svg.selectAll('#subhed').attr('font-size', '12px')
+  
+          }
+          
+          else if (newWidth > 250) {
+            draw(width, 5,20)
+            svg.selectAll('.steps').attr('x', 20)
+            svg.selectAll('.title').attr('x', -50)
+            svg.selectAll('#hed').attr('font-size', '15px')
+            svg.selectAll('#subhed').attr('font-size', '10px')
+
+        
+          } else {
+            draw(width, 5,20)
+            svg.selectAll('.steps').attr('x', 20)
+            svg.selectAll('.title').attr('x', -100)
+            svg.selectAll('#hed').attr('font-size', '15px')
+            svg.selectAll('#subhed').attr('font-size', '10px')
+
+          }
+          
+
+      }
+        window.addEventListener('resize', render)
+
+        // And now that the page has loaded, let's just try
+        // to do it once before the page has resized
+        render()
