852a5333849ba779e48bc083001010029c196711
kpananjady <51126772+kpananjady@users.noreply.github.com>
2020-11-02 18:02:54 -0500

    building

---
+++ src/scripts/2020_map_heading.js
@@ -0,0 +1,97 @@
+import * as d3 from 'd3'
+import * as topojson from 'topojson'
+import d3Tip from 'd3-tip'
+d3.tip = d3Tip
+
+let margin = { top: 0, left: 20, right: 20, bottom: 0 }
+
+let height = 150 - margin.top - margin.bottom
+
+let width = 1000 - margin.left - margin.right
+
+const colorScale = d3.scaleSequential(d3.interpolateOranges).domain([0, 5])
+
+let svg = d3
+  .select('#chart-0')
+  .append('svg')
+  .attr('height', height + margin.top + margin.bottom)
+  .attr('width', width + margin.left + margin.right)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+  d3.csv(require('../data/NH.csv')).then(ready)
+
+  function ready(dummydata) {
+
+    svg.append('text').attr('class', 'title').text('Results for the 2016 election are below').attr('alignment-baseline', 'middle').attr('y',45).attr('font-size', '25px').attr('font-weight', 5).attr('x', 0)
+    svg.append('text').attr('class', 'sub-title').text("Here's some chatter about the number of precincts reporting and the voter turnout").attr('alignment-baseline', 'middle').attr('y',75).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
+    svg.append('text').attr('class', 'sub-title').text('thus far. Results are unofficial and last updated at this time. ').attr('alignment-baseline', 'middle').attr('y',95).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
+
+
+
+    // svg.append('text').attr('id', 'box1-text').text('0').attr('x',width-220).attr('y', 100).attr('font-size', 10)
+    // svg.append('text').attr('id', 'box4-text').text('>800').attr('x',width-95).attr('y', 100).attr('font-size', 10)
+
+    // svg.append('rect').attr('id', 'box0').attr('width', 25).attr('height', 5).attr('x',width-175).attr('y', 100).attr('fill', colorScale(0)).attr('opacity',0.7)
+    // svg.append('rect').attr('id', 'box1').attr('width', 25).attr('height', 5).attr('x',width-150).attr('y', 100).attr('fill', colorScale(1)).attr('opacity',0.7)
+    // svg.append('rect').attr('id', 'box2').attr('width', 25).attr('height', 5).attr('x',width-125).attr('y', 100).attr('fill', colorScale(2)).attr('opacity',0.7)
+    // svg.append('rect').attr('id', 'box3').attr('width', 25).attr('height', 5).attr('x',width-100).attr('y', 100).attr('fill', colorScale(4)).attr('opacity',0.7)
+    // svg.append('rect').attr('id', 'box4').attr('width', 25).attr('height', 5).attr('x',width-75).attr('y', 100).attr('fill', colorScale(5)).attr('opacity',0.7)
+
+    // svg.append('rect').attr('id', 'box0-m').attr('width', 25).attr('height', 5).attr('x',width-175).attr('y', 100).style('fill', 'white').attr('opacity',0.4)
+    // svg.append('rect').attr('id', 'box1-m').attr('width', 25).attr('height', 5).attr('x',width-150).attr('y', 100).style('fill', 'white').attr('opacity',0.4)
+    // svg.append('rect').attr('id', 'box2-m').attr('width', 25).attr('height', 5).attr('x',width-125).attr('y', 100).style('fill', 'white').attr('opacity',0.4)
+    // svg.append('rect').attr('id', 'box3-m').attr('width', 25).attr('height', 5).attr('x',width-100).attr('y', 100).style('fill', 'white').attr('opacity',0.4)
+    // svg.append('rect').attr('id', 'box4-m').attr('width', 25).attr('height', 5).attr('x',width-75).attr('y', 100).style('fill', 'white').attr('opacity',0.4)
+
+    function render() {
+        const svgContainer = svg.node().closest('div')
+        const svgWidth = svgContainer.offsetWidth
+        // Do you want it to be full height? Pick one of the two below
+        const svgHeight = height + margin.top + margin.bottom
+        // const svgHeight = window.innerHeight
+    
+        const actualSvg = d3.select(svg.node().closest('svg'))
+        actualSvg.attr('width', svgWidth).attr('height', svgHeight)
+    
+        const newWidth = svgWidth - margin.left - margin.right
+        const newHeight = svgHeight - margin.top - margin.bottom
+
+
+        if (newWidth > 500) {
+
+          console.log('here')
+            svg.selectAll('.title').attr('font-size', '20px')
+            svg.selectAll('.sub-title').attr('font-size', '15px')
+        
+  
+          } else if (newWidth > 450){
+  
+            svg.selectAll('.title').attr('font-size', '15px')
+            svg.selectAll('.sub-title').attr('font-size', '10px')
+         
+
+          }
+          
+          else if (newWidth > 250) {
+
+            svg.selectAll('.title').attr('font-size', '12px')
+            svg.selectAll('.sub-title').attr('font-size', '9px')
+
+          }
+          
+          
+          else {
+         
+            svg.selectAll('.title').attr('font-size', '15px')
+            svg.selectAll('.sub-title').attr('font-size', '10px')
+
+          }  
+
+    }
+    window.addEventListener('resize', render)
+
+    // And now that the page has loaded, let's just try
+    // to do it once before the page has resized
+    render()
+  }
---
+++ src/scripts/2020_map_results.js
@@ -0,0 +1,472 @@
+
+import * as d3 from 'd3'
+import * as topojson from 'topojson'
+import d3Tip from 'd3-tip'
+d3.tip = d3Tip
+
+let margin = { top: 100, left: 50, right: 50, bottom: 0 }
+
+let height = 600 - margin.top - margin.bottom
+
+let width = 1000 - margin.left - margin.right
+
+const colorScale = d3.scaleSequential(d3.interpolateRdBu).domain([-10, 10])
+
+let svg = d3
+  .select('#chart-1')
+  .append('svg')
+  .attr('height', height + margin.top + margin.bottom)
+  .attr('width', width + margin.left + margin.right)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  const projection = d3.geoMercator()
+  .scale(width*17)
+            .center([-72.68, 41.1])
+            .translate([(width) / 2, (height)/2]);
+const path = d3.geoPath().projection(projection)
+
+const tip = d3
+.tip()
+.attr('class', 'd3-tip d3-tip-scrolly')
+.style('pointer-events', 'none')
+.offset([-10, 0])
+.html(function(d) {
+
+  var i=1;
+
+  var string_return = `${d.properties.newName}<br>`
+
+  for (i=1; i < d.properties[0] + 1; i++){
+    if (d.properties[i].includes('Democratic')){
+      string_return = string_return +`<span style='color:lightgrey'>${d.properties[i]},  <span style='color:lightblue'>${d.properties[`${i}_pct`]}% <br>`
+
+    } else if (d.properties[i].includes('Republican')){
+      string_return = string_return +`<span style='color:lightgrey'>${d.properties[i]},  <span style='color:pink'>${d.properties[`${i}_pct`]}% <br>`
+
+    }else if (d.properties[i].includes('Working')){
+      string_return = string_return +`<span style='color:lightgrey'>${d.properties[i]},  <span style='color:lightblue'>${d.properties[`${i}_pct`]}% <br>`
+
+    }
+    else {
+    string_return = string_return +`<span style='color:lightgrey'>${d.properties[i]},  <span style='color:#FFE4B5'>${d.properties[`${i}_pct`]}% <br>`
+    }
+
+  }
+
+ return string_return  
+
+})
+
+
+
+
+svg.call(tip)
+
+Promise.all([
+    d3.json(require('/data/ct_towns_simplified.topojson')),
+    d3.csv(require('/data/this_is_it_really.csv')),
+    d3.json(require('/data/ct_congressional_districts.json')),
+    d3.json(require('/data/ct_house_districts.json')),
+    d3.json(require('/data/ct_state_senate.json')),
+    d3.csv(require('/data/datapoints_options.csv')),
+    d3.csv(require('/data/election_2020/us_house.csv')),
+    d3.csv(require('/data/election_2020/ct_state_senate.csv')),
+    d3.csv(require('/data/election_2020/ct_state_house.csv'))
+
+  ]).then(ready)
+  .catch(err => console.log('Failed on', err))
+
+  function ready([json2, datapoints, d2, d_house, d_senate, datapoints3, us_house_data, ct_state_senate, ct_state_house]) {
+
+    const towns = topojson.feature(json2, json2.objects.townct_37800_0000_2010_s100_census_1_shp_wgs84)
+
+    var towns2 = svg
+    .selectAll('path-town')
+    .data(towns.features)
+    .enter()
+    .append('path')
+    .attr('class', 'path-town')
+    .attr('d', path)
+    .attr('fill', 'grey')  
+    .attr('stroke', 'white')  
+    .on('mouseover', function(d){
+      d.properties.name = d.properties.NAME10
+
+      tip.show.call(this, d)
+      // d3.select(this).attr('stroke', 'white').attr('opacity','0.5')
+  })
+
+
+
+    d3.select("#selectButton").on("change", function(d) {
+
+            // recover the option that has been chosen
+            var selectedOption = d3.select(this).property("value")
+            // run the updateChart function with this selected option
+            update(selectedOption)
+        })
+    
+        d3.select("#selectButton")
+        .selectAll('myOptions')
+         .data(datapoints3)
+        .enter()
+        .append('option')
+        .text(function (d) { return d.race; }) // text showed in the menu
+        .attr("value", function (d) { return d.race; })
+    
+    
+    
+        function update(selectedGroup) {
+
+          if (selectedGroup==='Presidential Election'){
+
+            svg.selectAll('.path-town').remove()
+
+                
+    var towns2 = svg
+    .selectAll('path-town')
+    .data(towns.features)
+    .enter()
+    .append('path')
+    .attr('class', 'path-town')
+    .attr('d', path)
+    .attr('fill', 'grey')  
+    .attr('stroke', 'white')  
+    .on('mouseover', function(d){
+      d.properties.name = d.properties.NAME10
+      tip.show.call(this, d)
+      // d3.select(this).attr('stroke', 'white').attr('opacity','0.5')
+  })
+
+          } else if (selectedGroup==='State Senate'){
+
+            svg.selectAll('.path-town').remove()
+
+
+      var senate_seats = svg
+      .selectAll('path-town')
+      .data(d_senate.features)
+      .enter()
+      .append('path')
+      .attr('class', 'path-town')
+      .attr('d', path)
+      .attr('fill', function(d){            
+        if (parseFloat(d.properties.name)<10){
+          var name_and_number = "State Senator 0" + d.properties.name
+          } else {
+            var name_and_number = "State Senator " + d.properties.name
+            }
+  
+
+      var dem_vote = 0;
+      var rep_vote=0;
+      ct_state_senate.forEach( function(r) {if (r['Office Name']===name_and_number){
+
+
+        if (r['variable'].includes("Democratic")){
+          console.log('dems')
+          console.log((100*r['value']/r['Vote Totals']).toFixed(2))
+          dem_vote = dem_vote + (100*r['value']/r['Vote Totals']).toFixed(2)
+        } else if (r['variable'].includes("Working")){
+          dem_vote = dem_vote + (100*r['value']/r['Vote Totals']).toFixed(2)
+
+        } else if (r['variable'].includes("Republican")){
+          rep_vote = rep_vote + (100*r['value']/r['Vote Totals']).toFixed(2)
+        }
+        // console.log(r['variable'], Math.round(100*r['value']/r['Vote Totals']))
+       }
+
+
+      }) 
+
+      console.log(dem_vote)
+      console.log(parseFloat(dem_vote)-parseFloat(rep_vote))
+      return colorScale(parseFloat(dem_vote)-parseFloat(rep_vote))
+
+    })
+    .attr('stroke', 'white')  
+    .on('mouseover', function(d){
+
+      console.log(d)
+      if (parseFloat(d.properties.name)<10){
+        var name_and_number = "State Senator 0" + d.properties.name
+        } else {
+          var name_and_number = "State Senator " + d.properties.name
+          }
+
+    console.log(name_and_number)
+
+  
+    var counter = 0
+    ct_state_senate.forEach( function(r) {if (r['Office Name']===name_and_number){
+      counter = counter +1 
+      // console.log(r['variable'], Math.round(100*r['value']/r['Vote Totals']))
+
+      d.properties[counter] = r['variable']
+      d.properties[`${counter}_pct`]=(100*r['value']/r['Vote Totals']).toFixed(2)
+      console.log(d.properties[`${counter}_pct`])
+      // d.properties.fid[counter] = r['value']
+      // console.log(d.properties[counter] )
+      // console.log(d.properties[1])
+     }
+    })
+    d.properties[0] = counter
+    d.properties.newName = name_and_number
+    tip.show.call(this, d)
+
+
+    })
+
+          } else if (selectedGroup==='State House'){
+
+            svg.selectAll('.path-town').remove()
+
+            var house_seats = svg
+            .selectAll('path-town')
+            .data(d_house.features)
+            .enter()
+            .append('path')
+            .attr('class', 'path-town')
+            .attr('d', path)
+            .attr('fill', function(d){            
+              if (parseFloat(d.properties.name)<10){
+                var name_and_number = "State Representative 0" + d.properties.name
+                } else {
+                  var name_and_number = "State Representative " + d.properties.name
+                  }
+
+            var dem_vote = 0;
+            var rep_vote=0;
+            ct_state_house.forEach( function(r) {if (r['Office Name']===name_and_number){
+      
+      
+              if (r['variable'].includes("Democratic")){
+                console.log('dems')
+                console.log((100*r['value']/r['Vote Totals']).toFixed(2))
+                dem_vote = dem_vote + (100*r['value']/r['Vote Totals']).toFixed(2)
+              } else if (r['variable'].includes("Working")){
+                dem_vote = dem_vote + (100*r['value']/r['Vote Totals']).toFixed(2)
+      
+              } else if (r['variable'].includes("Republican")){
+                rep_vote = rep_vote + (100*r['value']/r['Vote Totals']).toFixed(2)
+              }
+              // console.log(r['variable'], Math.round(100*r['value']/r['Vote Totals']))
+             }
+      
+      
+            })     
+            
+            console.log(dem_vote)
+            console.log(parseFloat(dem_vote)-parseFloat(rep_vote))
+            return colorScale(parseFloat(dem_vote)-parseFloat(rep_vote))
+          }) 
+          .attr('stroke', 'white')  
+          .on('mouseover', function(d){
+
+            console.log(d)
+            if (parseFloat(d.properties.name)<10){
+              var name_and_number = "State Representative 0" + d.properties.name
+              } else {
+                var name_and_number = "State Representative " + d.properties.name
+                }
+      
+      
+        
+          var counter = 0
+          ct_state_house.forEach( function(r) {if (r['Office Name']===name_and_number){
+            counter = counter +1 
+            // console.log(r['variable'], Math.round(100*r['value']/r['Vote Totals']))
+      
+            d.properties[counter] = r['variable']
+            d.properties[`${counter}_pct`]=(100*r['value']/r['Vote Totals']).toFixed(2)
+            console.log(d.properties[`${counter}_pct`])
+            // d.properties.fid[counter] = r['value']
+            // console.log(d.properties[counter] )
+            // console.log(d.properties[1])
+           }
+          })
+          d.properties[0] = counter
+          d.properties.newName = name_and_number
+          d.properties.fid = d.properties.name
+          tip.show.call(this, d)
+              // d3.select(this).attr('stroke', 'white').attr('opacity','0.5')
+          })
+
+          } else if (selectedGroup==='U.S. House'){
+
+            svg.selectAll('.path-town').remove()
+
+
+                  var cong_districts = svg
+      .selectAll('path-town')
+      .data(d2.features)
+      .enter()
+      .append('path')
+      .attr('class', 'path-town')
+      .attr('d', path) 
+    .attr('stroke', 'white')  
+    .on('mouseover', function(d){
+
+        var name_and_number = "Representative in Congress 0" + d.properties.fid
+
+        console.log(name_and_number)
+
+      
+        var counter = 0
+        us_house_data.forEach( function(r) {if (r['Office Name']===name_and_number){
+          counter = counter +1 
+          // console.log(r['variable'], Math.round(100*r['value']/r['Vote Totals']))
+
+          d.properties[counter] = r['variable']
+          d.properties[`${counter}_pct`]=(100*r['value']/r['Vote Totals']).toFixed(2)
+          console.log(d.properties[`${counter}_pct`])
+          // d.properties.fid[counter] = r['value']
+          // console.log(d.properties[counter] )
+          // console.log(d.properties[1])
+         }
+        })
+        d.properties[0] = counter
+        d.properties.newName = name_and_number
+
+        d.properties.name = d.properties.fid
+        tip.show.call(this, d)
+
+        // d3.select(this).attr('stroke', 'white').attr('opacity','0.5')
+    })
+    .attr('fill', function(d){
+      var name_and_number = "Representative in Congress 0" + d.properties.fid
+
+      var dem_vote = 0;
+      var rep_vote=0;
+      us_house_data.forEach( function(r) {if (r['Office Name']===name_and_number){
+
+
+        if (r['variable'].includes("Democratic")){
+          console.log('dems')
+          console.log((100*r['value']/r['Vote Totals']).toFixed(2))
+          dem_vote = dem_vote + (100*r['value']/r['Vote Totals']).toFixed(2)
+        } else if (r['variable'].includes("Working")){
+          dem_vote = dem_vote + (100*r['value']/r['Vote Totals']).toFixed(2)
+
+        } else if (r['variable'].includes("Republican")){
+          rep_vote = rep_vote + (100*r['value']/r['Vote Totals']).toFixed(2)
+        }
+        // console.log(r['variable'], Math.round(100*r['value']/r['Vote Totals']))
+       }
+
+
+      })     
+      
+      console.log(dem_vote)
+      console.log(parseFloat(dem_vote)-parseFloat(rep_vote))
+      return colorScale(parseFloat(dem_vote)-parseFloat(rep_vote))
+    }) 
+
+          }
+
+    }
+    
+  
+
+    function render() {
+      const svgContainer = svg.node().closest('div')
+      const svgWidth = svgContainer.offsetWidth
+      // Do you want it to be full height? Pick one of the two below
+      const svgHeight = height + margin.top + margin.bottom
+      // const svgHeight = window.innerHeight
+  
+      const actualSvg = d3.select(svg.node().closest('svg'))
+      actualSvg.attr('width', svgWidth).attr('height', svgHeight)
+  
+      const newWidth = svgWidth - margin.left - margin.right
+      const newHeight = svgHeight - margin.top - margin.bottom
+
+
+
+      if (newWidth > 500) {
+
+          console.log(newWidth, 'newWidth 1')
+          projection
+          .scale(newWidth*20)
+          .center([-72.68, 40.9])
+          .translate([(newWidth) / 2, (newHeight)/2]);
+
+          svg.selectAll('.path-town')
+      .attr('d', path)
+
+
+  
+      
+
+        } else if (newWidth > 487){
+
+        
+
+          projection
+        .scale(newWidth*26)
+        .center([-72.68, 41])
+
+        .translate([(newWidth) / 2, (newHeight)/2]);
+
+        svg.selectAll('.path-town')
+      .attr('d', path)
+
+     
+        } else if (newWidth >370) {
+
+          console.log(newWidth, 'newWidth 3')
+          projection
+          .center([-72.68, 41])
+          .scale(newWidth*30)
+          .translate([(newWidth) / 2, (newHeight)/2]);
+
+          svg.selectAll('.path-town')
+      .attr('d', path)
+
+
+        }
+        else if (newWidth > 250) {
+
+          console.log(newWidth, 'newWidth 3')
+          projection
+          .center([-72.68, 41])
+          .scale(newWidth*28)
+          .translate([(newWidth) / 2, (newHeight)/2]);
+
+          svg.selectAll('.path-town')
+      .attr('d', path)
+
+
+        }
+        else {
+          console.log(newWidth, 'newWidth 4')
+
+
+          svg.select('.school_name').attr('font-size', '15px')
+          
+          projection
+          .scale(svgWidth*25)
+          .center([-72.4, 40.4])
+          .translate([(svgWidth) / 2, (svgHeight)/2]);
+
+          svg.selectAll('.path-town')
+      .attr('d', path)
+        }
+        
+  }
+  window.addEventListener('resize', render)
+
+  // And now that the page has loaded, let's just try
+  // to do it once before the page has resized
+  render()
+    // console.log(data_moused_over)
+    // svg.select("#changing_percentage_2").text('In this town, X% of people like you returned their ballots.')
+    // data_moused_over.forEach(function(r){  
+    //   console.log(selectedGroup)
+    //   console.log(r['DT MAILED '])
+    //   if (selectedGroup===r['DT MAILED ']){
+    //     console.log('matched')
+    //     svg.select("#changing_percentage_2").text('In this town, ' + Math.round(r['percentage'])+'% of people like you returned their ballots.')
+    // } 
+  // })
+    }
