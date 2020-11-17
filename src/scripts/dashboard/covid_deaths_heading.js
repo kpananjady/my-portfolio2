6faf79225f0f7878605c70eed176102d0235f4da
kpananjady <51126772+kpananjady@users.noreply.github.com>
2020-11-17 16:06:44 -0500

    fixes

--- src/scripts/dashboard/covid_deaths_heading.js
+++ src/scripts/dashboard/covid_deaths_heading.js
@@ -23,7 +23,7 @@ let svg = d3
 
   function ready(dummydata) {
 
-    svg.append('text').attr('class', 'title').text('CT COVID hospitalizations on the rise').attr('alignment-baseline', 'middle').attr('y',45).attr('font-size', '25px').attr('font-weight', 5).attr('x', 0)
+    svg.append('text').attr('class', 'title').text('CT COVID deaths on the rise').attr('alignment-baseline', 'middle').attr('y',45).attr('font-size', '25px').attr('font-weight', 5).attr('x', 0)
     svg.append('text').attr('class', 'sub-title').text("While death tolls are smaller than in the peak of the pandemic, deaths").attr('alignment-baseline', 'middle').attr('y',75).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
     svg.append('text').attr('class', 'sub-title').text('tend to lag behind cases.').attr('alignment-baseline', 'middle').attr('y',95).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
 
