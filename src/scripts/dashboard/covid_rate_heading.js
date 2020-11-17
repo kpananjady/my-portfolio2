1e7863e571cb03645f07e02798efeb50373df37b
kpananjady <51126772+kpananjady@users.noreply.github.com>
2020-11-17 15:50:40 -0500

    all headings

--- src/scripts/dashboard/covid_rate_heading.js
+++ src/scripts/dashboard/covid_rate_heading.js
@@ -23,9 +23,9 @@ let svg = d3
 
   function ready(dummydata) {
 
-    svg.append('text').attr('class', 'title').text('Results for the 2020 election are below').attr('alignment-baseline', 'middle').attr('y',45).attr('font-size', '25px').attr('font-weight', 5).attr('x', 0)
-    svg.append('text').attr('class', 'sub-title').text("Choose your race and click on a district to see votes. 99.5% of precincts").attr('alignment-baseline', 'middle').attr('y',75).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
-    svg.append('text').attr('class', 'sub-title').text('are reporting data. Results were last updated at 7:20 a.m.').attr('alignment-baseline', 'middle').attr('y',95).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
+    svg.append('text').attr('class', 'title').text('Positivity rate average crosses 5%').attr('alignment-baseline', 'middle').attr('y',45).attr('font-size', '25px').attr('font-weight', 5).attr('x', 0)
+    svg.append('text').attr('class', 'sub-title').text("The positivity rate is the ratio of positive tests to total tests. The ").attr('alignment-baseline', 'middle').attr('y',75).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
+    svg.append('text').attr('class', 'sub-title').text('seven-day rolling average has crossed 5%.').attr('alignment-baseline', 'middle').attr('y',95).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
 
 
 
