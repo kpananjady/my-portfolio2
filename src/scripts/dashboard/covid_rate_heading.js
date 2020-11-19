04e16a334e9e7480075a551f6eb1f980ca63ce8a
kpananjady <51126772+kpananjady@users.noreply.github.com>
2020-11-19 16:51:47 -0500

    +ve rate

--- src/scripts/dashboard/covid_rate_heading.js
+++ src/scripts/dashboard/covid_rate_heading.js
@@ -24,7 +24,7 @@ let svg = d3
   function ready(dummydata) {
 
     svg.append('text').attr('class', 'title').text('CT COVID positivity rate over time').attr('alignment-baseline', 'middle').attr('y',45).attr('font-size', '25px').attr('font-weight', 5).attr('x', 0)
-    svg.append('text').attr('class', 'sub-title').text("The positivity rate is the ratio of positive tests to total tests. The ").attr('alignment-baseline', 'middle').attr('y',75).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
+    svg.append('text').attr('class', 'sub-title').text("The positivity rate is the ratio of COVID cases to total tests. The ").attr('alignment-baseline', 'middle').attr('y',75).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
     svg.append('text').attr('class', 'sub-title').text('data are represented based on the day a test result is reported.').attr('alignment-baseline', 'middle').attr('y',95).attr('font-size', '20px').attr('font-weight', 5).attr('x', 0)
 
 
